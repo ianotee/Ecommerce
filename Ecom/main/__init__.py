6d90d1c2b071baf8ec5cbaf9ee3a82747bf27cb2
ianotee <oteeian500@gmail.com>
2023-10-21 16:42:14 +0300

    first commit

---
+++ Ecom/main/__init__.py
@@ -0,0 +1,8 @@
+from python_daraja import payment
+# bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919 
+
+payment.SHORT_CODE = "174379"
+payment.PASSKEY = "bfb279f9aa9bdbcf158e97dd71a467cd2e0c893059b10f78e6b72ada1ed2c919"
+payment.CONSUMER_SECRET = "5MoyY30qY4PE9DGg"
+payment.CONSUMER_KEY = "hLZtQJUJNA9kdy5aBYQAf0BgDMfXtNrW"
+payment.ACCOUNT_TYPE = "TILL"
